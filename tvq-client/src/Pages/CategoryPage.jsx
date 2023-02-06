4f3d0de3b8e612fb45e9431a2f60bda59c6d5c88
iQuerz <niki.tenis@gmail.com>
2023-02-06 16:27:23 +0100

    Add category page & fix mediaItem title ui

---
+++ tvq-client/src/Pages/CategoryPage.jsx
@@ -0,0 +1,15 @@
+import { Typography, Box } from "@mui/material";
+import { useRef,useState } from "react";
+import { Link } from "react-router-dom";
+import LoginForm from "../Components/Login/LoginForm";
+
+
+function CategoryPage(props) {
+    return(
+        <Box className="flex-down" paddingTop={'5em'}>
+            <Typography variant="h1">Not implemented yet!</Typography>
+        </Box>
+    )
+}
+
+export default CategoryPage
