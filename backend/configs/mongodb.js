4ba6e20b538b280dd0fdfcac02614c6e331e2eb0
iQuerz <niki.tenis@gmail.com>
2023-02-12 21:38:38 +0100

    Add mongo atlas connection string

--- backend/configs/mongodb.js
+++ backend/configs/mongodb.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const connectDB = async () => {
     try {
         mongoose.set("strictQuery", false);
-        const conn = await mongoose.connect("mongodb://localhost:27017")
+        const conn = await mongoose.connect("mongodb+srv://tvqueue:tvqueue@cluster0.15xmjyt.mongodb.net/?retryWrites=true&w=majority")
         console.log(`MongoDB connected - ${conn.connection.host}:${conn.connection.port}`)
     } catch (error) {
         console.log(error)
